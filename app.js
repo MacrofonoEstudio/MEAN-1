843aad9726130388c1b64fd63c8879ae1c8be272
MacrofonoEstudio <info@macrofonoestudio.es>
2014-12-22 20:36:06 +0100

    Changed port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var port     = process.env.PORT || 8080; // set our port
+var port     = process.env.PORT || 8000; // set our port
 
 var mongoose = require('mongoose');
 //mongoose.connect(''); // connect to our database
