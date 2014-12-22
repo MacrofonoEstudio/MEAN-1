c9b878775866fb6f034f649a9b31d7fffe43294b
MacrofonoEstudio <info@macrofonoestudio.es>
2014-12-22 21:02:45 +0100

    + cambios

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(function(err, req, res, next) {
 
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("Node app is running at port:" + app.get('port'));
 });
 
 module.exports = app;
