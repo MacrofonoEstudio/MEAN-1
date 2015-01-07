57cc323ac328532dcf463e8b3f01c19a0368190a
MacrofonoEstudio <info@macrofonoestudio.es>
2015-01-07 13:08:12 +0100

    c

--- public/javascripts/passport-init.js
+++ public/javascripts/passport-init.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var User  = require('../../models/fideliausers');
 
 var FacebookStrategy = require('passport-facebook').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
 
 module.exports = function(passport){
@@ -47,6 +48,22 @@ module.exports = function(passport){
 				done(null, user);
 			});
 
+	    });
+	  }
+	));
+
+
+	passport.use(new LocalStrategy(
+	  function(username, password, done) {
+	    User.findOne({ username: username }, function(err, user) {
+	      if (err) { return done(err); }
+	      if (!user) {
+	        return done(null, false, { message: 'Incorrect username.' });
+	      }
+	      if (!user.validPassword(password)) {
+	        return done(null, false, { message: 'Incorrect password.' });
+	      }
+	      return done(null, user);
 	    });
 	  }
 	));
