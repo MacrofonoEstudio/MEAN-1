443d064177d9385a9d7df1c5b672ee1611919cc0
MacrofonoEstudio <info@macrofonoestudio.es>
2015-01-08 20:46:06 +0100

    Test

--- public/javascripts/passport-init.js
+++ public/javascripts/passport-init.js
@@ -58,8 +58,17 @@ module.exports = function(passport){
 	passport.use(new LocalStrategy(
 	  function(username, password, done) {
 	    User.findOne({ email: username }, function(err, user) {
+	      if (err) { return done(err); }
 	      
-	    	console.log('Inside findOne');
+	      /* (!user) {
+	        return done(null, false, { message: 'Incorrect username.' });
+	      }
+	      if (!user.validPassword(password)) {
+	        return done(null, false, { message: 'Incorrect password.' });
+	      }
+	      return done(null, user);
+			*/////
+		console.log('Inside findOne');
 
 	    });
 	  }
