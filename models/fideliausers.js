306ad1d7b1a2856e41b2740db39480284cdb0a22
MacrofonoEstudio <info@macrofonoestudio.es>
2015-01-14 19:41:27 +0100

    c

--- models/fideliausers.js
+++ models/fideliausers.js
@@ -4,7 +4,7 @@ var Schema       = mongoose.Schema;
 var fideliappSchema   = new Schema({
 	name : String, // Nombre del usuario
 	provider: String, // Facebook o Email
-	provider_id : {type: String, unique: true}, // ID que proporciona Twitter o Facebook...
+	provider_id : {type: String}, // ID que proporciona Twitter o Facebook...
 	photo : String, // Avatar o foto del usuario
 	gender : String,
 	email : String,
