cf54af241ce0a77910aac89fae88e7043c71764c
MacrofonoEstudio <info@macrofonoestudio.es>
2015-01-18 18:48:25 +0100

    c

--- models/fideliausers.js
+++ models/fideliausers.js
@@ -1,6 +1,11 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
+// Encrypt and "I forgot" password
+var bcrypt = require('bcrypt-nodejs');
+var async = require('async');
+var crypto = require('crypto'); // Node integrated library that generates random token 
+
 var fideliappSchema   = new Schema({
 	name : String, // Nombre del usuario
 	provider: String, // Facebook o Email
@@ -10,7 +15,35 @@ var fideliappSchema   = new Schema({
 	email : String,
 	password: String,
 	verify: Boolean,
+	resetPasswordToken: String, // Código aleatorio generado por Crypto para verificar el usuario a la hora de generar un nuevo password
+  	resetPasswordExpires: Date, // Tiempo que va a estar activo el código para renovar el password
 	createdAt : {type: Date, default: Date.now} // Fecha de creación
 });
+
+// We modify the Mongoose Schema to encrypt the passwords in the save action
+fideliappSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Adds to the schema a method that compares passwords to verify
+fideliappSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('fideliaUser', fideliappSchema);
