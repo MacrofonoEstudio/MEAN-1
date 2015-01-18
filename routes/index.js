c14478b5eaf20d125c0f99625209f1a78885390d
MacrofonoEstudio <info@macrofonoestudio.es>
2015-01-18 19:28:05 +0100

    c

--- routes/index.js
+++ routes/index.js
@@ -56,12 +56,11 @@ router.get('/auth/facebook/logged', passport.authenticate('facebook', { successR
 
 // Passport Local user/pw
 router.post('/login',
-	passport.authenticate('local', { successRedirect: '', failureRedirect: '/login' }),
+	passport.authenticate('local', { successRedirect: '', failureRedirect: '/loginfail' }),
 	function(req, res){ // When visiting this route first Passport tries to authentificate the user, if ok it proceeds to the next function
 
 	console.log('Usuario registrado y logeado' + req.user.name);
 	res.render('index', {usuario: req.user.name});
-
 
 });
 
